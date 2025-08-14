ef67f64e27d8e3975622272d4ee48dd39ce32719
Deondre2002 <dj232011@gmail.com>
2025-08-14 16:50:41 -0400

    Add files via upload

--- patterns/accumulator.js
+++ patterns/accumulator.js
@@ -22,8 +22,16 @@ export function sumToN(n) {
  * @returns `1` if n is 0
  */
 export function factorial(n) {
- if (n === 0) || n === 1) {
-  return 1; 
+ if (typeof n !== "number") return NaN;
+  if (n < 0) return undefined;
+  if (n === 0 || n === 1) return 1;
+
+  let product = 1;
+  for (let i = 2; i <= n; i++) {
+    product *= i;
+  }
+
+  return product;
 }
 
 /**
